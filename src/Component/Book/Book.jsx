3424fa542a03dd2d88d1c4013c92389e4bf23472
Ashik70021 <ashik70021@gmail.com>
2024-03-26 03:09:18 +0600

    load data to the books section

--- src/Component/Book/Book.jsx
+++ src/Component/Book/Book.jsx
@@ -1,21 +1,31 @@
+
+import { CiStar } from "react-icons/ci";
 import { Link } from "react-router-dom";
 
-const Book = () => {
+const Book = ({ book }) => {
+    const { img, tags, book_name, author, category, rating } = book;
     return (
         <Link to=''>
             <div className="card w-96 bg-base-100 shadow-xl">
-                <figure className="px-10 pt-10">
-                    <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />
-                </figure>
+                <div className="p-6">
+                    <img src={img} alt="Shoes" className="rounded-xl"/>
+                </div>
                 <div className="card-body text-center">
                     <div className="flex">
-                        <h1 className=" pl-2 pr-2 pb-1 pt-1 bg-[#e8ffe5] text-[#23BE0A] rounded-3xl text-xl font-semibold mr-6">tags1 asdas</h1>
-                        <h1 className=" pl-2 pr-2 pb-1 pt-1 bg-[#e8ffe5] text-[#23BE0A] rounded-3xl text-xl font-semibold mr-6">tags2 asfas</h1>
+                        <h1 className=" pl-2 pr-2 pb-1 pt-1 bg-[#e8ffe5] text-[#23BE0A] rounded-3xl text-xl font-semibold mr-6">{tags[0]}</h1>
+                        <h1 className=" pl-2 pr-2 pb-1 pt-1 bg-[#e8ffe5] text-[#23BE0A] rounded-3xl text-xl font-semibold mr-6">{tags[1]}</h1>
                     </div>
-                    <h2 className="card-title font-bold text-2xl text-[#131313]">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions">
-                        <button className="btn btn-primary">Buy Now</button>
+                    <h2 className="card-title font-bold text-2xl text-[#131313]">{book_name}</h2>
+                    <p className="card-title text-[#131313cc] font-medium text-lg">By: {author}</p>
+                    <hr className="mt-2 dashed" />
+                    <div className="flex justify-around mt-4">
+                        <div>
+                            <span className="text-[#131313cc] font-medium text-lg">{category}</span>
+                        </div>
+                        <div className="flex gap-3">
+                            <p className="text-lg text-[#131313cc]">{rating}</p>
+                            <div className="text-2xl text-[#131313cc]"><CiStar /></div>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/Component/Books/Books.jsx
+++ src/Component/Books/Books.jsx
@@ -1,11 +1,23 @@
 import Book from "../Book/Book";
-
+import { useEffect, useState } from "react";
 const Books = () => {
+    const [books, setBooks] = useState([]);
+
+    useEffect(()  =>{
+        fetch('books.json')
+        .then(res => res.json())
+        .then(data => setBooks(data));
+    })
     return (
         <div>
-            <h2 className="font-bold text-3xl text-center ml-6">Books</h2>
-            <div>
-                <Book></Book>
+            <h2 className="font-bold text-3xl text-center ml-6">Books: {books.length}</h2>
+            <div className="grid grid-cols-3 gap-8 mt-16">
+                {
+                    books.map(book => <Book 
+                        key={book.id} 
+                        book={book} >
+                        </Book> )
+                }
             </div>
         </div>
     );
