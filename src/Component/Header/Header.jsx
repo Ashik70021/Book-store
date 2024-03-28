126d5bdc7ddfd70622e10b6ad7d8276b78f92f14
Ashik70021 <ashik70021@gmail.com>
2024-03-28 16:41:16 +0600

    solve navbar dropdown button

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -2,36 +2,34 @@ import { Link, NavLink } from "react-router-dom";
 
 
 const Header = () => {
-
-    const links = <>
-        <li className="text-xl"><NavLink to="/">Home</NavLink></li>
-        <li className="text-xl"><NavLink to="/listedbooks">Listed Books</NavLink></li>
-        <li className="text-xl"><NavLink to="/pagetoread">Pages to Read</NavLink></li>
-        <li className="text-xl"><NavLink to="/orderbook">Order Book</NavLink></li>
-        <li className="text-xl"><NavLink to="/contact">Contact</NavLink></li>
-    </>
-
     return (
-        <div className="navbar mt-6 ">
+        <div className="navbar bg-base-100">
             <div className="navbar-start">
                 <div className="dropdown">
-                    <div role="button" className="btn btn-ghost lg:hidden">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </div>
-                    <ul className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                        {links}
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        <li className="text-xl"><NavLink to="/">Home</NavLink></li>
+                        <li className="text-xl"><NavLink to="/listedbooks">Listed Books</NavLink></li>
+                        <li className="text-xl"><NavLink to="/pagetoread">Pages to Read</NavLink></li>
+                        <li className="text-xl"><NavLink to="/orderbook">Order Book</NavLink></li>
+                        <li className="text-xl"><NavLink to="/contact">Contact</NavLink></li>
                     </ul>
                 </div>
                 <Link to='/'><a className="text-2xl">Book Vibe</a></Link>
-                
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                    {links}
+                    <li className="text-xl"><NavLink to="/">Home</NavLink></li>
+                    <li className="text-xl"><NavLink to="/listedbooks">Listed Books</NavLink></li>
+                    <li className="text-xl"><NavLink to="/pagetoread">Pages to Read</NavLink></li>
+                    <li className="text-xl"><NavLink to="/orderbook">Order Book</NavLink></li>
+                    <li className="text-xl"><NavLink to="/contact">Contact</NavLink></li>
                 </ul>
             </div>
-            <div className="navbar-end gap-3">
-                <a className="btn text-white font-bold text-lg bg-[#23BE0A]">Sign In</a>
+            <div className="navbar-end">
+            <a className="btn text-white font-bold text-lg bg-[#23BE0A]">Sign In</a>
                 <a className="btn  text-white font-bold text-lg bg-[#59C6D2]">Sign Up</a>
             </div>
         </div>
